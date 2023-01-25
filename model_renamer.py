3010347a1b9b9f8fed1775a9a5b99011fd48fea2
Thomas Van Iseghem <thomas.van.iseghem@gmail.com>
2023-01-25 20:42:28 +0100

    Added 2.0.0 ModelRepo / better naming for cabs

--- model_renamer.py
+++ model_renamer.py
@@ -4,11 +4,11 @@ import argparse
 import xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(description='Rename the models in an XML file')
-parser.add_argument('xml_file', metavar='xml_file', type=str, help='The XML file to be parsed')
-parser.add_argument('out_file', metavar='output_file', type=str, default='ModelRepo_generated.xml', help='The output file', const='ModelRepo_generated.xml', nargs='?')
+parser.add_argument('--original', metavar='xml_file', type=str, help='The XML file to be parsed')
+parser.add_argument('--out', metavar='output_file', type=str, default='ModelRepo_generated.xml', help='The output file', const='ModelRepo_generated.xml', nargs='?')
 args = parser.parse_args()
 
-tree = ET.parse(args.xml_file)
+tree = ET.parse(args.original)
 root = tree.getroot()
 
 def get_actual_model(model):
@@ -19,6 +19,10 @@ def get_actual_model(model):
     new_name = trademark.replace('Based on ', '')
     # remove "®" from the name
     new_name = new_name.replace('®', '')
+    # Change out the word "loaded" for "|"
+    new_name = new_name.replace('loaded with', '|')
+    # remove the word "with" from "loaded with"
+    new_name = new_name.replace('with', '|')
 
     # Check if the name contains a Mono or Stereo suffix
     if name.endswith('(M)'):
@@ -29,7 +33,7 @@ def get_actual_model(model):
     return new_name
 
 
-print('Renaming models in file: ' + args.xml_file)
+print('Renaming models in file: ' + args.original)
 # Itterate over the categories inside the XML file
 for category in root.findall('Category'):
     # Itterate over the models inside the category
@@ -40,9 +44,9 @@ for category in root.findall('Category'):
             new_name = get_actual_model(model)
             model.set('name', new_name)
 
-print('Saving file to: ' + args.out_file)
+print('Saving file to: ' + args.out)
 # Save the XML file
-tree.write(args.out_file, encoding='UTF-8', xml_declaration=True)
+tree.write(args.out, encoding='UTF-8', xml_declaration=True)
 
 
 
