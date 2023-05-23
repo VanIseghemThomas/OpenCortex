a5a395cce2d24928b11735f315c231dd104acd71
Thomas Van Iseghem <thomas.vaniseghem@spectralbot.com>
2023-05-23 18:04:55 +0200

    Created a better slimmed down emulation container

--- QC-dev-environment/filesystems/README.md
+++
@@ -1,7 +0,0 @@
-# Update name registry
-
-Update download files are found inside `/media/p4/update-<update-name>.bin`
-
-**2.0.0:** update-vm44GCkrMujYR8o47OkF8UW.AdVYyu0U
-
-**2.0.1:** update-cIR97IrfvF4fKfO5_KIm_j_aP2zGiHp_
--- QC-dev-environment/install_qt_src.sh
+++
@@ -1,4 +0,0 @@
-# First download the package using wget
-wget -LO https://src.fedoraproject.org/repo/pkgs/qt/qt-everywhere-opensource-src-4.8.7.tar.gz/d990ee66bf7ab0c785589776f35ba6ad/qt-everywhere-opensource-src-4.8.7.tar.gz
-# Then extract it
-tar -zxvf qt-everywhere-opensource-src-4.8.7.tar.gz
